b073fe6ef03eba5365f096f6aed938ffc7f9233e
wdeaderick <wbd4@rice.edu>
2017-07-05 19:18:39 -0500

    Update runDLSI.m

--- runDLSI.m
+++ runDLSI.m
@@ -1,19 +1,19 @@
-function [D, pred, f1s, Xts] = runDLSI(Y, Yrange, Yts, totalpat)
+function [D, pred, f1s, Xts] = runDLSI(Y, Yrange, Yts, totalpat,eta,lambda)
 %Change DLSI input parameters here
 fprintf('Running DLSI... \n')
-k = 20;   %Number of dictionary atoms
+k = 300;   %Number of dictionary atoms
 C              = 2;
 D_range        = k*(0:C);
 opts.k = k;
-opts.lambda    = 0.001;
-opts.eta       = 0.01;
+opts.lambda    = lambda;
+opts.eta       = eta;
 opts.D_range   = D_range;
 opts.show_cost = 0;
 opts.show      = 0;
 opts.verbose    = false;
 opts.max_iter  = 10; 
 tic
-[D, X, rt] = DLSI(Y, Yrange, opts);
+[D, ~, ~] = DLSI(Y, Yrange, opts);
 toc
 Xts = lasso_fista(Yts, D, [],opts.lambda, opts);
 a = DLSI_pred(Yts, D, opts);
@@ -25,7 +25,7 @@ for i = 1:size(Yts,2)/totalpat
     f1s = [f1s; sum(a(lookvec) == 1 )]; %Confidence of prediction
     pred = [pred; temp];
 end
-
+ 
 %{
 % Use to code chunk to view Dictionary atoms
 m = size(D,2)/10;
