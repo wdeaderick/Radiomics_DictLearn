13b113ebdde5a4848464b9904e4e652de95a3227
wdeaderick <wbd4@rice.edu>
2017-07-05 19:19:28 -0500

    Update runCOPAR.m

--- runCOPAR.m
+++ runCOPAR.m
@@ -1,25 +1,25 @@
-function [D, pred, f1s] = runCOPAR(Y, Yrange, Yts, totalpat)
+function [D, pred, f1s] = runCOPAR(Y, Yrange, Yts, totalpat,eta,lambda)
 %Change COPAR input parameters here
 fprintf('Running COPAR... \n')
-k = 20;
-k0 = 10;
+k = 150;
+k0 = k;
 opts.k = k;
 opts.k0 = k0;
 C                = 2;
 D_range_ext = [k*(0:C), k*C + k0];
-opts.lambda      = 0.001;
-opts.eta         = 0.01;    
+opts.lambda      = lambda;
+opts.eta         = eta;   
 opts.show        = false;
 opts.max_iter    = 10;        
 opts.verbose      = false;
-
+ 
 tic
-[D, X, rt] = COPAR(Y, Yrange, opts);
+[D, ~, ~] = COPAR(Y, Yrange, opts);
 toc
 opts.classify_mode = 'LC'; %'LC' or 'GC'
 opts.gamma = 0.01; %[0.0001, 0.001, 0.005, 0.01]
 a = COPAR_pred(Yts, D, D_range_ext, opts);
-
+ 
 pred = []; f1s = [];
 a = a-1;
 for i = 1:size(Yts,2)/totalpat
